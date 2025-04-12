133d27d502119fd97a4d2429c2d87500e9932048
yogichip <151203539+yogichip@users.noreply.github.com>
2025-04-12 13:28:25 -0700

    added comparator for SemesterSchedule

--- dev-00-0.2/ReadMe.txt
+++ dev-00-0.2/ReadMe.txt
@@ -3,7 +3,7 @@
 
 # who did what:
 1. Eman Naseerkhan - Made Saved Office Hours Schedule UI and worked on displaying data
-2. Yogi Chipalkatti -
+2. Yogi Chipalkatti - worked on sorting SemesterSchedule by date and timeslot
 3. Bhuvanesh Selvaraj -
 3. Nhat Nguyen -
 
--- dev-00-0.2/src/main/java/s25/cs151/application/SemesterSchedule.java
+++ dev-00-0.2/src/main/java/s25/cs151/application/SemesterSchedule.java
@@ -33,7 +33,42 @@ public class SemesterSchedule implements Comparable<SemesterSchedule> {
     // To do: sort data ascending on date and time slot
     @Override
     public int compareTo(SemesterSchedule o) {
-        return 0;
+        try {
+
+            java.time.format.DateTimeFormatter formatter = java.time.format.DateTimeFormatter.ofPattern("mm/dd/yyyy");
+            java.time.LocalDate thisDate = java.time.LocalDate.parse(this.getDate(), formatter);
+            java.time.LocalDate otherDate = java.time.LocalDate.parse(o.getDate(), formatter);
+
+
+            int dateComparison = thisDate.compareTo(otherDate);
+            if (dateComparison != 0) {
+                return dateComparison;
+            }
+
+
+            String thisFromHour = this.getTimeSlot().split("-")[0].trim();
+            String otherFromHour = o.getTimeSlot().split("-")[0].trim();
+
+            String[] thisParts = thisFromHour.split(":");
+            String[] otherParts = otherFromHour.split(":");
+
+            int thisHour = Integer.parseInt(thisParts[0]);
+            int thisMinute = Integer.parseInt(thisParts[1]);
+
+            int otherHour = Integer.parseInt(otherParts[0]);
+            int otherMinute = Integer.parseInt(otherParts[1]);
+
+
+            if (thisHour != otherHour) {
+                return thisHour - otherHour;
+            } else {
+                return thisMinute - otherMinute;
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing date or timeslot", e);
+        }
+
     }
 
 }
